097890c54e119bf7206830fbb2627d2c44e9f037
DoffyInstitutes <140860229+DoffyInstitutes@users.noreply.github.com>
2025-07-26 10:54:08 -0500

    Delete Beginner directory

--- Beginner/Day 3/Modulo/task.py
+++
@@ -1,8 +0,0 @@
-value = input("What is the number? ")
-value = int(value)
-if value % 2 == 0:
-    print("This value is even")
-else:
-    print("This value is odd")
-#more efficient code solution
-#typecast in input line from start; modulus in conditional
